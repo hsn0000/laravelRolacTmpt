662869110727c60053ae5e08a5da1caed3eca512
MHusin <Muhamad.husin120495@gmail.com>
2019-08-26 17:00:49 +0700

    f

---
+++ resources/views/siswa/index.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+ <div class="container">
+    <div class="row">
+     <div class="col-6">
+      <h1>Data Siswa</h1> 
+      </div>
+     <div class="col-6">
+            <!-- Button trigger modal -->
+            <br>
+     <button type="button" class="btn btn-primary float-right btn-sm" data-toggle="modal" data-target="#exampleModal">
+     Tambah Data Siswa
+    </button>
+    </div> 
+       <table class="table table-hover ">
+         <tr>
+            <th>Nama Depan</th>
+            <th>Nama Belakang</th>
+            <th>Jenis Kelamin</th>
+            <th>Agama</th>
+            <th>Alamat</th>
+        </tr>
+        @foreach($data_siswa as $siswa)
+        <tr>
+            <td> {{$siswa->nama_depan}} </td>
+            <td> {{$siswa->nama_belakang}} </td>
+            <td> {{$siswa->jenis_kelamin}} </td>
+            <td> {{$siswa->agama}} </td>
+            <td> {{$siswa->alamat}} </td>
+        @endforeach
+
+         </tr>
+      </table>   
+    </div>
+ </div>
+   
+   <!-- Modal -->
+   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+         <div class="modal-body">
+            ...
+         </div>
+           <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+     </div>
+ </div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> 
+</body>
+</html>
+
+
